be5b724a68283a482ef0b6b8b13175b1200d0e81
Ginger69bro <brodersen2006@gmail.com>
2025-01-06 13:59:17 +0100

    Update Roman.java
    penis

--- work/Roman.java
+++ work/Roman.java
@@ -1,12 +1,9 @@
 public class Roman {
     public static final String[]
-            SYMBOLE = {
-            "M", "CM", "D", "C", "L", "X", "V", "I"
-    };
+            SYMBOLE = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    
     public static final int[]
-            NUMMER = {
-             1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
-    };
+            NUMMER = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
     public static String ausgabe = "";
 
