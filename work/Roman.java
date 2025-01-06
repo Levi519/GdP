cefa232711c4029f50cfd9098b1b647f491a8f7f
Levi519 <levi.bubenzer2@gmail.com>
2025-01-06 13:57:08 +0100

    Rekursive Methode fertig

--- work/Roman.java
+++ work/Roman.java
@@ -1,22 +1,22 @@
 public class Roman {
     public static final String[]
             SYMBOLE = {
-            "M", "D", "C", "L", "X", "V", "I"
+            "M", "CM", "D", "C", "L", "X", "V", "I"
     };
     public static final int[]
             NUMMER = {
-             1000, 500, 100, 50, 10, 5, 1
+             1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
     };
 
-    static String roman(int n) {
-        String result = new String();
-        for(int i=0; i<NUMMER.length; i++){
-            while (n >= NUMMER[i]){
-               result += (SYMBOLE[i]);
-                n -= NUMMER[i];
-            }
+    public static String ausgabe = "";
+
+    static void roman(int n, int index) {
+        if (n == 0) return;
+        while (n >= NUMMER[index]) {
+            ausgabe += SYMBOLE[index];
+            n -= NUMMER[index];
         }
-        return result;
+        roman(n, index + 1);
     }
     public static void main (String [] args) {
         if (args.length == 0) {
@@ -24,10 +24,10 @@ public class Roman {
             return;
         }
 
-
         int N = Integer.parseInt(args[0]);
         if (1 <= N && N <= 5000) {
-            System.out.println(roman(N));
+            roman(N, 0);
+            System.out.println(ausgabe);
         } else {
             System.out.println("Die Zahl muss zwischen 1 und 5000 liegen.");
         }
