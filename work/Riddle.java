a065d0bf8f8417909584145918842b17d77ed2a2
Levi519 <levi.bubenzer2@gmail.com>
2025-01-12 17:11:27 +0100

    Überarbeitet

--- work/Riddle.java
+++ work/Riddle.java
@@ -5,59 +5,62 @@ public class Riddle {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         solveRiddle(n);
-        if (n > 10){
+        if (n > 5){
             System.out.println(count + " Loesungen");
-        }else if(n == 3){
+        }else if(n == 3 || n == 4){
             System.out.println("eine Loesung");
         }
     }
 
     public static void solveRiddle(int n) {
+        // Wenn n ausserhalb des vorgegebenen Bereichs liegt
         if (n <= 0 || n > 15) {
-            System.out.println("keine Loesung");
+            System.out.println("Bitte Zahl zwischen 1 und 15 wählen");
             return;
         }
 
-        int[] field = new int[2 * n]; // Das Feld, in dem die Zahlen platziert werden
-        boolean hasSolution = findSolutions(field, n, 1);
+        int[] feld = new int[2 * n];
+        boolean hatLoesung = loesen(feld, n, 1);
 
-        if (!hasSolution) {
+        // Fehlermeldung keine Loesung ausgeben
+        if (!hatLoesung) {
             System.out.println("keine Loesung");
         }
     }
 
     // Rekursive Methode zur Suche nach Lösungen
-    public static boolean findSolutions(int[] field, int n, int currentNumber) {
-        if (currentNumber > n) {
+    public static boolean loesen(int[] feld, int n, int position) {
+        if (position > n) {
             if (n > 10){
                 count++;
             }else {
-                printSolution(field);
+                count++;
+                ausgeben(feld);
             }
             return true; // Eine Lösung wurde gefunden
         }
 
-        boolean found = false;
+        boolean loesung = false;
 
-        for (int i = 0; i < field.length - currentNumber - 1; i++) {
-            if (field[i] == 0 && field[i + currentNumber + 1] == 0) { // Prüfen, ob die Plätze frei sind
-                field[i] = currentNumber;
-                field[i + currentNumber + 1] = currentNumber;
+        for (int i = 0; i < feld.length - position - 1; i++) {
+            if (feld[i] == 0 && feld[i + position + 1] == 0) { // Prüfen, ob die Plätze frei sind
+                feld[i] = position;
+                feld[i + position + 1] = position;
 
-                found |= findSolutions(field, n, currentNumber + 1);
+                loesung |= loesen(feld, n, position + 1);
 
-                // Backtracking: Zurücksetzen der Felder
-                field[i] = 0;
-                field[i + currentNumber + 1] = 0;
+                // Zurücksetzen der Felder
+                feld[i] = 0;
+                feld[i + position + 1] = 0;
             }
         }
-        return found;
+        return loesung;
     }
 
-    // Methode zum Drucken einer Lösung
-    public static void printSolution(int[] solution) {
-        for (int num : solution) {
-            System.out.print(num);
+    // Methode zum Ausgeben einer Lösung
+    public static void ausgeben(int[] solution) {
+        for (int i = 0; i < solution.length; i++) {
+            System.out.print(solution[i]);
         }
         System.out.println();
     }
